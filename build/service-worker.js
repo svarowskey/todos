f48194d56dd054b49dca5164cc121391cfd03c3f
svarowskey <vladimir.svarowski@gmail.com>
2020-08-25 23:32:52 +0600

    Rebuild project

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9fe26508d3039d091aa2aa43ba509d6.js"
+  "/precache-manifest.046b6c503cca89973af75ca31f9ee4a8.js"
 );
 
 self.addEventListener('message', (event) => {
